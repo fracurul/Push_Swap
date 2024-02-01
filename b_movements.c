fa9ec0b95ac13839db0ad43f7ad5b9f8d3f3506d
Franco Stefano Curulla Galan <fracurul@c2r3s3.42malaga.com>
2024-02-01 21:11:17 +0100

    movements

--- b_movements.c
+++ b_movements.c
@@ -1,53 +1,71 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   b_movements.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 18:17:24 by fracurul          #+#    #+#             */
+/*   Updated: 2024/02/01 20:09:30 by fracurul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_sb(t_stack *stack_b)
+void	sb_mov(t_stack *stack_b)
 {
 	t_node	*swap;
 
+	if (!stack_b || !stack_b->head || stack_b->head->next)
+		return ;
 	swap = stack_b->head;
 	stack_b->head = stack_b->head->next;
 	stack_b->head->next = swap;
 	swap->next = NULL;
 }
 
-void	ft_pb(t_stack *stack_a, t_stack *stack_b)
+void	pb_mov(t_stack *stack_a, t_stack *stack_b)
 {
-	t_node *swap;
+	t_node	*swap;
 
+	if (!stack_a || !stack_a->head || stack_a->head->next)
+		return ;
 	swap = stack_a->head;
 	stack_a->head = stack_a->head->next;
 	stack_b = swap;
 	swap->next = NULL;
 }
 
-void	ft_rb(t_stack *stack_b)
+void	rb_mov(t_stack *stack_b)
 {
-	t_node *swap;
-	t_node *rotation;
+	t_node	*swap;
+	t_node	*rotation;
 
+	if (!stack_b || !stack_b->head || stack_b->head->next)
+		return ;
 	swap = stack_b->head;
 	rotation = stack_b->head;
-	while(rotation->next)
+	while (rotation->next)
 		rotation = rotation->next;
 	rotation->next = swap;
 	swap->next = NULL;
 }
 
-void ft_rrb(t_stack *stack_b)
+void	rrb_mov(t_stack *stack_b)
 {
-	t_node *prev_node;
-	t_node *r_rotation;
+	t_node	*prev_node;
+	t_node	*r_rotation;
 
+	if (!stack_b || !stack_b->head || stack_b->head->next)
+		return ;
 	prev_node = NULL;
 	r_rotation = stack_b->head;
-	while(r_rotation->next)
+	while (r_rotation->next)
 	{
 		prev_node = r_rotation;
 		r_rotation = r_rotation->next;
 	}
 	prev_node->next = NULL;
 	r_rotation->next = stack_b->head;
-	stack_b->head = r_rotation; 
+	stack_b->head = r_rotation;
 }
