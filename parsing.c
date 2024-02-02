ea53f38bbbca2a50b687d71c42d1d0120b2a0dbe
Franco Stefano Curulla Galan <fracurul@c3r9s3.42malaga.com>
2024-02-02 19:44:36 +0100

    parse

---
+++ parsing.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 18:31:48 by fracurul          #+#    #+#             */
+/*   Updated: 2024/02/02 19:42:34 by fracurul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+char	*ft_param_check(int argc, char **argv)
+{
+
+}
+int	*t_parse(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	int	size;
+	int	*values;
+
+	i = 0;
+	j = 0;
+	while(argv[i][j])
+		i++;
+}
