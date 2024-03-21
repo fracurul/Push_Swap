e283eff01fbf15653d6a974db24e262a8aea32f6
Franco Stefano Curulla Galan <fracurul@c3r1s3.42malaga.com>
2024-03-21 21:17:16 +0100

    algorithm

--- algorithm.c
+++ algorithm.c
@@ -1,5 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 18:49:16 by fracurul          #+#    #+#             */
+/*   Updated: 2024/03/21 21:04:22 by fracurul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "push_swap.h"
+
+/**
+ * @brief Get the max value object.
+ *
+ * @param stack
+ * @return int with the max value.
+ */
+int	get_max_value(t_stack *stack)
+{
+	int		max_value;
+	t_node *current;
+
+	current = stack->head;
+	max_value = current->max_value;
+	while(!current)
+	{
+		if(current->max_value > max_value)
+			max_value = current->max_value;
+		current = current->next;
+	}
+	return (max_value);
+}
 
 int	is_sorted(t_stack *stack)
 {
@@ -36,4 +69,16 @@ void	sort_stacks(t_stack **stack_a, t_stack **stack_b)
 	if(size_a-- > 3 && is_sorted(*stack_a))
 		pb_mov(stack_a, stack_b);
 	while(size_a-- > 3 && is_sorted(*stack_a))
+	{
+		nodes_init(*stack_a, *stack_b);
+		move_to_b(stack_a, stack_b);
+	}
+	sort3(stack_a);
+	while(*stack_b)
+	{
+		nodes_init_b(*stack_a, *stack_b);
+		move_to_a(stack_a, stack_b);
+	}
+	current_median(*stack_a);
+	min_to_top(stack_a);
 }
